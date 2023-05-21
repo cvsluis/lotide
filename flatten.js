2f784ecd6229bd08dc5ccc16ab0fbfee1e25c3fd
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-05-20 23:47:09 -0700

    Function that takes in array with elements and nested arrays of elements and returns single array

---
+++ flatten.js
@@ -0,0 +1,37 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  } else {
+    return false;
+  }
+};
+
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+const flatten = function(array) {
+  let newArray = [];
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      for (let j = 0; j < array[i].length; j++) {
+        newArray.push(array[i][j]);
+      }
+    } else {
+      newArray.push(array[i]);
+    }
+  }
+  return newArray;
+};
+
+assertArraysEqual((flatten([1, 2, [3, 4], 5, [6]])), [1, 2, 3, 4, 5, 6]);
