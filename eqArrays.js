2da5c46661267e3e1dfc201879ce78afa5105fe9
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-05 15:50:40 -0700

    Refactor eqArrays, export function and move tests to eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,4 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
-const eqArrays = function(array1,array2) {
+const eqArrays = function(array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   }
@@ -18,9 +10,4 @@ const eqArrays = function(array1,array2) {
   return true;
 };
 
-assertEqual((eqArrays([1, 2, 3], [1, 2, 3])), true);
-assertEqual((eqArrays([1, 2, 3], [3, 2, 1])), false);
-
-assertEqual((eqArrays(["1", "2", "3"], ["1", "2", "3"])), true);
-assertEqual((eqArrays(["1", "2", "3"], ["3", "2", "1"])), false);
-
+module.exports = eqArrays;
