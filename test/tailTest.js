8c59cb2c2d48a5e49d8c8d764f02e989115bb6d6
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-05 16:52:38 -0700

    Implement mocha and chai for assertions

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,18 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
 
-const result = tail(["Lighthouse", "Labs", "Bootcamp"]);
-assertEqual(result[1], "Bootcamp");
-assertEqual(result[0], "Labs");
+describe("#tail", () => {
+  it("returns 3 for words.length", () => {
+    assert.strictEqual(words.length, 3);
+  });
+
+  it("returns 2 for tail(words).length", () => {
+    assert.strictEqual(tail(words).length, 2);
+  });
+
+  it("returns ['Lighthouse', 'Labs'] for tail(words)", () => {
+    assert.deepEqual((tail(words)), ['Lighthouse', 'Labs']);
+  });
+});
