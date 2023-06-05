5f0532a3a306989e7b722de3105f00853ac7edb5
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-05 16:07:06 -0700

    Refactor middle and move tests to middleTest

--- middle.js
+++ middle.js
@@ -1,45 +1,14 @@
-const eqArrays = function(array1,array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const middle = function(array) {
   let newArray = [];
-  // array with two or less elements
   if (array.length <= 2) {
     return newArray;
-    // array with odd number of elements
-  } else if (array.length % 2) {
-    // add the element of the mid index to newArray
-    newArray.push(array[Math.floor(array.length / 2)]);
-    return newArray;
-    // add the two middle elements to the newArray
-  } else {
+  }
+  if (array.length % 2 === 0) {
     newArray.push(array[Math.floor(array.length / 2) - 1], array[Math.floor(array.length / 2)]);
     return newArray;
   }
+  newArray.push(array[Math.floor(array.length / 2)]);
+  return newArray;
 };
 
-assertArraysEqual((middle([1])), []); // => []
-assertArraysEqual((middle([1, 2])), []); // => []
-
-assertArraysEqual((middle([1, 2, 3])), [2]); // => [2]
-assertArraysEqual((middle([1, 2, 3, 4, 5])), [3]); // => [3]
-
-assertArraysEqual((middle([1, 2, 3, 4])), [2, 3]); // => [2, 3]
-assertArraysEqual((middle([1, 2, 3, 4, 5, 6])), [3, 4]);// => [3, 4]
+module.exports = middle;
