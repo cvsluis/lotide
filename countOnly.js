66d76492a8d9468a54bbb027015e2860a26d7c39
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-06 15:02:42 -0700

    Added comment

--- countOnly.js
+++ countOnly.js
@@ -1,3 +1,4 @@
+// Function that takes in a collection of items and returns a specific subset of those items
 const countOnly = function(allItems, itemsToCount) {
   const results = {};
   for (const item of allItems) {
