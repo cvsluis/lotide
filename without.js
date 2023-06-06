7d999136ed47a652b2a816836b2a188091468048
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-06 13:46:06 -0700

    Refactor without and move tests to withoutTest

--- without.js
+++ without.js
@@ -1,41 +1,11 @@
-const eqArrays = function(array1,array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  }
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1, arr2)) {
-    console.log(`✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
-
 const without = function(array, itemsToRemove) {
   const newArray = [];
-  for (let i = 0; i < array.length; i++) {
-    for (let j = 0; j < itemsToRemove.length; j++) {
-      if ((array[i] !== itemsToRemove[j]) && (typeof(array[i]) === typeof(itemsToRemove[j]))) {
-        newArray.push(array[i]);
-      }
+  array.forEach(item => {
+    if (!itemsToRemove.includes(item)) {
+      newArray.push(item);
     }
-  }
+  })
   return newArray;
 };
 
-
-// TESTS
-assertArraysEqual((without([1, 2, 3], [1])), [2, 3]);
-assertArraysEqual((without(["1", "2", "3"], [1, 2, "3"])), ["1", "2"]);
-
-const words = ["hello", "world", "lighthouse"];
-without(words, ["lighthouse"]); // no need to capture return value for this test case
-// Make sure the original array was not altered by the without function
-assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+module.exports = without;
