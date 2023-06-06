dfb81bc41911a8cb804204d10c056824b2493568
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-05 23:32:40 -0700

    Refactor findKeyByValue and move tests to findKeyByValueTest

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const findKeyByValue = function(objectList, value) {
   for (const key in objectList) {
     if (objectList[key] === value) {
@@ -14,13 +6,4 @@ const findKeyByValue = function(objectList, value) {
   }
 };
 
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "Sherlock"), undefined);
+module.exports = findKeyByValue;
