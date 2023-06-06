c59dd9827836984bc45ee68963a3ba780144fa49
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-06 14:07:10 -0700

    Implemented recursion to support sub-arrays, and updated testing

--- eqArrays.js
+++ eqArrays.js
@@ -3,7 +3,11 @@ const eqArrays = function(array1, array2) {
     return false;
   }
   for (let i = 0; i < array1.length; i++) {
-    if (array1[i] !== array2[i]) {
+    if (Array.isArray(array1[i]) || Array.isArray(array2[i])){
+      if (!eqArrays(array1[i], array2[i])) {
+        return false;
+      }
+    } else if (array1[i] !== array2[i]) {
       return false;
     }
   }
