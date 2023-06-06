5c897488ee0b1139a79938e348cf1786726b22d4
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-06 15:13:21 -0700

    Added comments

--- eqObjects.js
+++ eqObjects.js
@@ -1,10 +1,11 @@
 const eqArrays = require('./eqArrays');
 
+// Function that compares two objects and returns true if equal, or false if not equal
+// Supports objects of arrays by using eqArrays function, and sub-objects by using recursion
 const eqObjects = function(object1, object2) {
   if (Object.keys(object1).length !== Object.keys(object2).length) {
     return false;
   }
-
   for (let key in object1) {
     if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
       if (!eqArrays(object1[key], object2[key])) {
