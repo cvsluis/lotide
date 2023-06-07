60bdf8cccf2409cd305d23bd11bb23470b17b41c
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-06 21:04:52 -0700

    Refactored flatten an added comments

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,3 +1,4 @@
+// Function that takes in an object and a value and returns the first key that contains that value
 const findKeyByValue = function(objectList, value) {
   for (const key in objectList) {
     if (objectList[key] === value) {
--- flatten.js
+++ flatten.js
@@ -1,14 +1,14 @@
+// Function that takes in an array containing a nested array of elements and returns a flattened version of the array
+// Supports unlimited nesting by using recursion
 const flatten = function(array) {
   let newArray = [];
-  for (let i = 0; i < array.length; i++) {
-    if (Array.isArray(array[i])) {
-      for (let j = 0; j < array[i].length; j++) {
-        newArray.push(array[i][j]);
-      }
+  array.forEach(i => {
+    if (Array.isArray(i)) {
+      flatten(i).forEach(j => newArray.push(j));
     } else {
-      newArray.push(array[i]);
+      newArray.push(i);
     }
-  }
+  }) 
   return newArray;
 };
 
