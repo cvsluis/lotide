ecaeac3627178ff11b0402141de8e1deec91dce2
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-07 14:55:32 -0700

    Removed old test code and reformatted

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,14 +1,6 @@
 const assert = require('chai').assert;
 const eqArrays = require('../eqArrays');
 
-// assertEqual((eqArrays([1, 2, 3], [1, 2, 3])), true);
-// assertEqual((eqArrays([1, 2, 3], [3, 2, 1])), false);
-// assertEqual((eqArrays(["1", "2", "3"], ["1", "2", "3"])), true);
-// assertEqual((eqArrays(["1", "2", "3"], ["3", "2", "1"])), false);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false);
-// assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false);
-
 describe("#eqArrays", () => {
   it("returns true when passed [1, 2, 3] and [1, 2, 3]", () => {
     assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
@@ -22,5 +14,4 @@ describe("#eqArrays", () => {
   it("returns true when passed two empty arrays", () => {
     assert.strictEqual(eqArrays([], []), true);
   });
-
 });
