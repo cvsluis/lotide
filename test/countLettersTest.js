f7cc923dd0489c64ab8f066b9a954fea578425df
Caitlin Vandersluis <caitlinvandersluis@gmail.com>
2023-06-07 15:46:38 -0700

    Updated testing to implement mocha and chai (stretch)

--- test/countLettersTest.js
+++ test/countLettersTest.js
@@ -1,16 +1,14 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const countLetters = require('../countLetters');
 
-const result1 = (countLetters('lighthouse in the house'));
-console.log(result1);
-
-assertEqual(result1["l"], 1);
-assertEqual(result1["i"], 2);
-assertEqual(result1["g"], 1);
-assertEqual(result1["h"], 4);
-assertEqual(result1["t"], 2);
-assertEqual(result1["o"], 2);
-assertEqual(result1["u"], 2);
-assertEqual(result1["s"], 2);
-assertEqual(result1["e"], 3);
-assertEqual(result1["n"], 1);
+describe("#countLetters", () => {
+  it("returns { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 } when passed 'lighthouse in the house'", () => {
+    assert.deepEqual(countLetters("lighthouse in the house"), { l: 1, i: 2, g: 1, h: 4, t: 2, o: 2, u: 2, s: 2, e: 3, n: 1 });
+  });
+  it("returns 4 for the letter h when passed 'lighthouse in the house'", () => {
+    assert.deepEqual(countLetters("lighthouse in the house").h, 4);
+  });
+  it("returns an empty object for an empty string", () => {
+    assert.deepEqual(countLetters(""), {});
+  });
+});
--- test/findKeyTest.js
+++ test/findKeyTest.js
@@ -1,22 +1,22 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const findKey = require('../findKey');
 
-const object1 = {
-  "Blue Hill": { stars: 1 },
-  "Akaleri":   { stars: 3 },
-  "noma":      { stars: 2 },
-  "elBulli":   { stars: 3 },
-  "Ora":       { stars: 2 },
-  "Akelarre":  { stars: 3 }
-};
-
-const object2 = {
-  "Portugal": { capital: "Lisbon" },
-  "The Netherlands": { capital: "Amsterdam" },
-  "Canada": { capital: "Ottawa" }
-};
-
-assertEqual((findKey(object1, x => x.stars === 2)), 'noma');
-assertEqual((findKey(object1, x => x.stars === 1)), 'Blue Hill');
-assertEqual((findKey(object1, x => x.stars === 4)), undefined);
-assertEqual((findKey(object2, x => x.capital === "Amsterdam")), 'The Netherlands');
+describe("#findKey", () => {
+  const object1 = {
+    "Blue Hill": { stars: 1 },
+    "Akaleri":   { stars: 3 },
+    "noma":      { stars: 2 },
+    "elBulli":   { stars: 3 },
+    "Ora":       { stars: 2 },
+    "Akelarre":  { stars: 3 }
+  };
+  it("returns noma when passed object1, and x => x.stars === 2", () => {
+    assert.strictEqual(findKey(object1, x => x.stars === 2), 'noma');
+  });
+  it("returns Blue Hill when passed object1, and x => x.stars === 1", () => {
+    assert.strictEqual(findKey(object1, x => x.stars === 1), 'Blue Hill');
+  });
+  it("returns undefined when passed object1, and x => x.stars === 4", () => {
+    assert.strictEqual(findKey(object1, x => x.stars === 4), undefined);
+  });
+});
